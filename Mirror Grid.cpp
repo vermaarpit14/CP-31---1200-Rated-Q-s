5d4359be166b82fa5431be6d16ec127c509f2269
vermaarpit14 <vermaarpit253@gmail.com>
2025-07-21 02:02:20 +0530

    solved 2

--- Mirror Grid.cpp
+++ Mirror Grid.cpp
@@ -38,33 +38,32 @@ int32_t main() {
     int t;
     cin>>t;
     while(t--){
-        int n, m;
-        cin>>n>>m;
-        vector<int> a(m);
-        for(int i=0 ; i<m ; i++){
-            cin>>a[i];
-        }
-        sort(all(a));
-        vector<int> gaps(m);
-        gaps[0] = (n-a[m-1]+a[0]-1);
-        for(int i=1 ; i<m ; i++){
-            gaps[i] = (a[i]-a[i-1]-1);
-        }
-        sort(rall(gaps));
-        int daysPassed=0, saved=0;
-        for(int i=0 ; i<m ; i++){
-            gaps[i] = gaps[i] - 2*daysPassed;
-            if(gaps[i] > 0){
-                saved += 1;
-            }
-            gaps[i] -= 2;
-            daysPassed += 1;
-            if(gaps[i] > 0){
-                saved += gaps[i];
-                daysPassed += 1;
+        int n;
+        cin>>n;
+        vector<vector<int>> grid(n, vector<int>(n, 0));
+        for (int i = 0; i < n; ++i)
+        {
+            string s;
+            cin >> s;
+            for (int j = 0; j < n; ++j)
+            {
+                grid[i][j] = s[j] - '0';
             }
         }
-        cout<<n-saved<<"\n";
+        int operations = 0;
+        for (int i = 0; i < (n + 1) / 2; ++i)
+        {
+            for (int j = 0; j < n / 2; ++j)
+            {
+                int x1 = i, y1 = j;
+                int x2 = j, y2 = n - 1 - i;
+                int x3 = n - 1 - i, y3 = n - 1 - j;
+                int x4 = n - 1 - j, y4 = i;
+                int sum = grid[x1][y1] + grid[x2][y2] + grid[x3][y3] + grid[x4][y4];
+                operations += min(sum, 4 - sum);
+            }
+        }
+        cout << operations << "\n";
     }
     return 0;
 }
